e6239baa4f7ed6d6aabc225bb793318be36c5944
Luca-Giudice <2giudice4@gmail.com>
2022-12-13 16:28:52 +0100

    snack1

---
+++ script.js
@@ -0,0 +1,29 @@
+// SNACK 1
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
+// SNACK 2
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa. (vietato usare includes() )
+// SNACK BONUS
+// L'utente inserisce un numero di 4 cifre in un input. Calcola la somma di tutte le cifre che compongono il numero e stampala in pagina.
+// Esempio: l'utente inserisce 1234, noi stampiamo 10.
+// Create una sotto-cartella per ogni jsnack e riportate la consegna all'inizio del file js.
+// La struttura dei file dovrebbe essere la seguente:
+// jsnack1 (cartella)
+// - index.html
+// - script.js
+// jsnack2 (cartella)
+// - index.html
+// - script.js
+// e così via
+
+
+
+// SNACK 1
+
+const es1Element = document.getElementById('es1')
+const number = parseInt(prompt('scegli un numero')trim())
+
+es1Element.innerText = number
+
+
+
+
